e346ef778d8e5c5f89a0098393a1d5a3cc0822e7
Aaron Harvey <aaron.harvey@fairchildsemi.com>
2015-02-26 13:55:04 -0500

    Simplified function call.  Reduced by ~100 bytes.
    Added README.md

--- micro-query.js
+++ micro-query.js
@@ -1,30 +1,26 @@
 /*!
- *  uQuery - A tiny and super fast url param parser.
+ *  uQuery - a fast and very small url parameter parser.
  *  0.0.1
  *  (c) 2015 Aaron Harvey - aaron.harvey@fairchildsemi.com
  *  MIT
  */
+
 /* exported uQuery */
-window.uQuery = (function() {
+window.uQuery = function(needle) {
     'use strict';
     var regex = /([^&=]+)=?([^&]*)/g;
+    var search = window.location.search.substring(1);
+    var hash = window.location.hash.split("?");
+    hash.shift();
 
-    function getQueryString(needle) {
-        var search = window.location.search.substring(1);
-        var hash = window.location.hash.split("?");
-        hash.shift();
+    var queryString = search || hash.join('?');
+    if (!queryString) return undefined;
 
-        var queryString = search || hash.join('?');
-
-        if (!queryString) return undefined;
-
-        var match;
-        while ((match = regex.exec(queryString))) {
-            if (decodeURIComponent(match[1]) === needle)
-                return decodeURIComponent(match[2]);
-        }
+    var match;
+    while ((match = regex.exec(queryString))) {
+        if (decodeURIComponent(match[1]) === needle)
+            return decodeURIComponent(match[2]);
     }
-    return {
-        get: getQueryString
-    };
-}());
+};
+
+/* TODO: Test against a cached version that preparses the url. */
